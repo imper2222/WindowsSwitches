8cdc41657c282b6ff0a01a1f979fe39282d3562d
imper2222 <imper2222@gmail.com>
2019-02-15 15:45:30 +0100

    Add project files.

---
+++ WindowsSwitches/FmMain.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace WindowsSwitches
+{
+    partial class FmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.dtpTime = new System.Windows.Forms.DateTimePicker();
+            this.btStart = new System.Windows.Forms.Button();
+            this.btStop = new System.Windows.Forms.Button();
+            this.nudLeftTime = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nudLeftTime)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Godzina";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Czas (minuty)";
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // dtpTime
+            // 
+            this.dtpTime.CustomFormat = "HH:mm tt";
+            this.dtpTime.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpTime.Location = new System.Drawing.Point(88, 22);
+            this.dtpTime.Name = "dtpTime";
+            this.dtpTime.ShowUpDown = true;
+            this.dtpTime.Size = new System.Drawing.Size(54, 20);
+            this.dtpTime.TabIndex = 7;
+            this.dtpTime.ValueChanged += new System.EventHandler(this.dtpTime_ValueChanged);
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(148, 19);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(157, 23);
+            this.btStart.TabIndex = 9;
+            this.btStart.Text = "Zaplanuj wyłączenie";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // btStop
+            // 
+            this.btStop.Location = new System.Drawing.Point(148, 48);
+            this.btStop.Name = "btStop";
+            this.btStop.Size = new System.Drawing.Size(157, 23);
+            this.btStop.TabIndex = 10;
+            this.btStop.Text = "Przerwij";
+            this.btStop.UseVisualStyleBackColor = true;
+            this.btStop.Click += new System.EventHandler(this.btStop_Click);
+            // 
+            // nudLeftTime
+            // 
+            this.nudLeftTime.Location = new System.Drawing.Point(88, 48);
+            this.nudLeftTime.Maximum = new decimal(new int[] {
+            1440,
+            0,
+            0,
+            0});
+            this.nudLeftTime.Name = "nudLeftTime";
+            this.nudLeftTime.Size = new System.Drawing.Size(54, 20);
+            this.nudLeftTime.TabIndex = 11;
+            this.nudLeftTime.ValueChanged += new System.EventHandler(this.nudLeftTime_ValueChanged);
+            // 
+            // FmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(323, 90);
+            this.Controls.Add(this.nudLeftTime);
+            this.Controls.Add(this.btStop);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.dtpTime);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FmMain";
+            this.Text = "WindowsSwitches";
+            ((System.ComponentModel.ISupportInitialize)(this.nudLeftTime)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Button btStop;
+        internal System.Windows.Forms.DateTimePicker dtpTime;
+        private System.Windows.Forms.NumericUpDown nudLeftTime;
+    }
+}
+
---
+++ WindowsSwitches/FmMain.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsSwitches
+{
+    public partial class FmMain : Form
+    {
+        private bool uruchomionoWylaczanie = false;
+
+        public FmMain()
+        {
+            InitializeComponent();
+            UstawDostepnoscKontrolek();
+            //dtpTime.Value = DateTime.Now;
+            nudLeftTime.Value = 45;
+            timer.Start();          
+        }
+
+        private void WylaczKomputer()
+        {
+            //MessageBox.Show("Wyłączono komputer");
+            Process.Start("shutdown", "/s /t 0");
+        }
+
+        private void UstawCzasWOparciuOGodzine()
+        {
+            nudLeftTime.ValueChanged -= nudLeftTime_ValueChanged;
+            if ((int)dtpTime.Value.TimeOfDay.TotalMinutes >= (int)DateTime.Now.TimeOfDay.TotalMinutes)
+                nudLeftTime.Value = (int)dtpTime.Value.TimeOfDay.TotalMinutes - (int)DateTime.Now.TimeOfDay.TotalMinutes;
+            else
+                nudLeftTime.Value = 1440 + ((int)dtpTime.Value.TimeOfDay.TotalMinutes - (int)DateTime.Now.TimeOfDay.TotalMinutes);
+            nudLeftTime.ValueChanged += nudLeftTime_ValueChanged;
+        }
+
+        private void UstawGodzineWOparciuOCzas()
+        {
+            dtpTime.ValueChanged -= dtpTime_ValueChanged;
+            dtpTime.Value = new DateTime(new DateTime(1753, 01, 01).Ticks + (DateTime.Now.TimeOfDay + new TimeSpan(0, (int)nudLeftTime.Value, 0)).Ticks);
+            dtpTime.ValueChanged += dtpTime_ValueChanged;
+        }
+
+        private void UstawDostepnoscKontrolek()
+        {
+            btStart.Enabled = !uruchomionoWylaczanie;
+            btStop.Enabled = uruchomionoWylaczanie;
+            dtpTime.Enabled = !uruchomionoWylaczanie;
+            nudLeftTime.Enabled = !uruchomionoWylaczanie;
+        }
+
+        private void btStart_Click(object sender, EventArgs e)
+        {           
+            uruchomionoWylaczanie = true;
+            UstawDostepnoscKontrolek();
+        }
+
+        private void btStop_Click(object sender, EventArgs e)
+        {           
+            uruchomionoWylaczanie = false;
+            UstawDostepnoscKontrolek();
+        }
+
+        private void teTime_EditValueChanged(object sender, EventArgs e)
+        {
+            UstawCzasWOparciuOGodzine();
+        }
+
+        private void teLeftTime_EditValueChanged(object sender, EventArgs e)
+        {
+            UstawGodzineWOparciuOCzas();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (uruchomionoWylaczanie)
+            {
+                UstawCzasWOparciuOGodzine();
+                if ((int)DateTime.Now.TimeOfDay.TotalMinutes == (int)dtpTime.Value.TimeOfDay.TotalMinutes)
+                {
+                    uruchomionoWylaczanie = false;
+                    WylaczKomputer();
+                }
+            }
+        }
+
+        private void dtpTime_ValueChanged(object sender, EventArgs e)
+        {
+            UstawCzasWOparciuOGodzine();
+        }
+
+        private void nudLeftTime_ValueChanged(object sender, EventArgs e)
+        {
+            UstawGodzineWOparciuOCzas();
+        }
+    }
+}
