58dc41a3dfccf14894b6a7c4dfcad1db252c428c
imper2222 <imper2222@gmail.com>
2019-02-27 09:24:57 +0100

    WPF

---
+++ WindowsSwitchesWPF/Controls/NumericUpDown.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WindowsSwitchesWPF.Controls
+{
+    /// <summary>
+    /// Interaction logic for NumericUpDown.xaml
+    /// </summary>
+    public partial class NumericUpDown : UserControl
+    {
+        private int _numValue = 0;
+
+        public int NumValue
+        {
+            get { return _numValue; }
+            set
+            {
+                _numValue = value;
+                txtNum.Text = value.ToString();
+            }
+        }
+
+        public NumericUpDown()
+        {
+            InitializeComponent();
+            txtNum.Text = _numValue.ToString();
+        }
+
+        private void cmdUp_Click(object sender, RoutedEventArgs e)
+        {
+            NumValue++;
+        }
+
+        private void cmdDown_Click(object sender, RoutedEventArgs e)
+        {
+            NumValue--;
+        }
+
+        private void txtNum_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txtNum == null)
+            {
+                return;
+            }
+
+            if (!int.TryParse(txtNum.Text, out _numValue))
+                txtNum.Text = _numValue.ToString();
+        }
+
+        private void txtNum_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+            txtNum.SelectAll();
+            txtNum.Focus();
+        }
+    }
+}
---
+++ WindowsSwitchesWPF/Controls/PlayButton.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WindowsSwitchesWPF.Controls
+{
+    /// <summary>
+    /// Interaction logic for PlayButton.xaml
+    /// </summary>
+    public partial class PlayButton : UserControl
+    {
+        public PlayButton()
+        {
+            InitializeComponent();
+        }
+
+        private void Viewbox_MouseEnter(object sender, MouseEventArgs e)
+        {
+            mainEllipse.Fill = new SolidColorBrush(Colors.DarkGray);
+            mainEllipse.Fill = new SolidColorBrush(Colors.DarkGray);
+        }
+
+        private void Viewbox_MouseLeave(object sender, MouseEventArgs e)
+        {
+            mainEllipse.Fill = new SolidColorBrush(Colors.Gray);
+            mainEllipse.Stroke = new SolidColorBrush(Colors.Gray);
+        }
+
+        private void Viewbox_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Color color = Colors.Gray;
+            color.R -= 50;
+            color.G -= 50;
+            color.B -= 50;
+            mainEllipse.Fill = new SolidColorBrush(color);
+            mainEllipse.Stroke = new SolidColorBrush(Colors.Transparent);
+        }
+
+        private void Viewbox_MouseUp(object sender, MouseButtonEventArgs e)
+        {           
+            mainEllipse.Fill = new SolidColorBrush(Colors.Gray);
+            mainEllipse.Stroke = new SolidColorBrush(Colors.Gray);
+        }
+    }
+}
